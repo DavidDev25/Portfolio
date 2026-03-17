14817c9f80c02cb040d87a1303955696a1c8f3f4
DavidDev25 <david.werner@david-werner.dev>
2026-03-17 11:49:36 +0100

    chore: update immutable package to version 5.1.5 and remove unused vite.svg file; add GitHub and LinkedIn links to Navbar and added github and LinkedIn Images to public folder

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -13,6 +13,8 @@ export function Navbar() {
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
+                <li><a href="https://github.com/daviddev25" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/david-werner-01a88032a/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
         </nav>
     );
